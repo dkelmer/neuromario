41a77455833f6b825fea9305c78208f579ec1cb8
Giorgio <gp922@nyu.edu>
2016-05-02 21:52:16 -0400

    hacks on hacks on hacks: wrote java TCP client and python TCP neural net server for huge performance boosts

---
+++ competition/research/kp/KPAgentTCP.java
@@ -0,0 +1,104 @@
+package competition.research.kp;
+
+import ch.idsia.ai.agents.Agent;
+import ch.idsia.mario.environments.Environment;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.SimpleScriptContext;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import org.python.util.PythonInterpreter;
+import org.python.core.*;
+import java.io.*;
+import java.net.*;
+
+/**
+ * Created by giorgio on 4/25/16.
+ */
+public class KPAgentTCP implements Agent {
+    protected String name = "Kelmer&Pizzorni KPAgent";
+    Socket socket;
+    OutputStreamWriter osw;
+
+    public void reset() {}
+
+    public boolean[] getAction(Environment observation) {
+        byte[][] lvlSceneObs = getAreaAroundMario(observation, 3, 2);
+        byte[] message = new byte[lvlSceneObs.length + lvlSceneObs[0].length];
+        String world = "";
+        String response = "";
+
+        for(int i = 0; i < lvlSceneObs.length; i++){
+            for(int j = 0; j < lvlSceneObs[0].length; j++) {
+                world = world + " " + (lvlSceneObs[i][j]);
+                message[i+j] = lvlSceneObs[i][j];
+            }
+        }
+        world += "\n";
+        int len = world.length();
+        boolean action[] = {false, false, false, false, false};
+        try {
+            socket = new Socket("localhost", 2016);
+            socket.getOutputStream().write(world.getBytes("US-ASCII"));
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            response = inFromServer.readLine();
+            System.out.println("Response: " + response);
+            socket.close();
+        }
+        catch(Exception ignore){}
+
+        String[] stringArray = response.split(" ");
+        //boolean[] intArray = new int[stringArray.length];
+        for (int i = 0; i < stringArray.length; i++) {
+            String numberAsString = stringArray[i];
+            int temp = Integer.parseInt(numberAsString);
+            if(temp == 1){
+                action[i] = true;
+            }
+            else{
+                action[i] = false;
+            }
+            System.out.println("Action:" + action[i]);
+        }
+        //socket.close();
+
+        return action;
+    }
+
+    public AGENT_TYPE getType() {
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+    }
+
+    public byte[][] getAreaAroundMario(Environment observation, int xWidth, int yHeight) {
+        int marioX = 11;
+        int marioY = 11;
+        byte[][] area = new byte[yHeight*2][xWidth*2+1];
+        byte[][] levelObservation = observation.getMergedObservationZ(1,0);
+        int xLoc = 0;
+        int yLoc = 0;
+        for (int y = -yHeight; y < yHeight; y++) {
+            xLoc = 0;
+            for (int x = -xWidth; x <= xWidth; x++) {
+//                System.out.printf("(lvlX, lvlY): (%d, %d)\n", marioX+x, marioY+y);
+//                System.out.printf("(xLoc, yLoc): (%d, %d)\n", xLoc, yLoc);
+//                System.out.printf("lvlObservation[marioY+y][marioX+x]: %d\n",levelObservation[marioY+y][marioX+x]);
+                area[yLoc][xLoc] = levelObservation[marioY+y][marioX+x];
+//                System.out.printf("area[yLoc][xLoc] = %d\n", area[yLoc][xLoc]);
+                xLoc++;
+            }
+            yLoc++;
+        }
+        return area;
+    }
+}
---
+++ competition/research/kp/server.py
@@ -0,0 +1,41 @@
+import socket
+import sys
+import numpy as np
+import keras
+from keras.models import model_from_json
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.utils import np_utils
+
+print "Running server on port 2016..."
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 2016)
+sock.bind(server_address)
+sock.listen(1)
+print "Server listening on port 2016"
+test = "0 1 0 0 1"
+print "Loading neural net from file..."
+nb_classes = 5
+net = model_from_json(open('/Users/giorgio/projects/neuromario/competition/research/kp/astar_arch.json').read())
+net.load_weights('/Users/giorgio/projects/neuromario/competition/research/kp/astar_weights.h5')
+net.compile(loss = 'mse', optimizer = RMSprop(), metrics = ['accuracy'])
+print "Neural net loaded and ready to go"
+
+try:
+  while True:
+    connection,client_address = sock.accept()
+    data = connection.recv(128)
+    x = np.array(data.split(), dtype = int)
+    x = x.reshape(28,1)
+    x = x.transpose()
+    y = net.predict_on_batch(x)[0]
+    moves = [1 if z > 0 else -1 for z in y]
+
+    response = " ".join(map(str,moves))
+    if data:
+      connection.sendall(response + "\n")
+    else:
+      break
+finally:
+  connection.close()
